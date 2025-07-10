24be72d30894f01b6bd3859f5b012a27c0d5ee28
Rodolfo <rowitt89@gmail.com>
2025-07-10 16:49:53 -0300

    codigo finalizado

--- mini_sistema_de_banco.py
+++ mini_sistema_de_banco.py
@@ -1,7 +1,7 @@
 menu = """
 
 [d] Deposit
-[w] withdraw
+[w] Withdraw
 [e] Extract
 [q] Quit
 
@@ -9,52 +9,46 @@ menu = """
 
 balance = 0
 limit = 1000
-balance = ""
-withdraw_amount = 0
+extract = ""
+withdraw_number = 0
 WITHDRAW_LIMIT = 5
 
 while True:
-
     option = input(menu)
 
-    if option = "d":
-        value = float(input ("Insert value to be deposited: "))
+    if option == "d":
+        value = float(input("Insert value to be deposited: "))
         if value > 0:
             balance += value
-            balance += f"Deposit: U$ {value:.2f}\n"
-
+            extract += f"Deposit: U$ {value:.2f}\n"
+            print("Deposit made successfully!")
         else:
             print("Operation failed, the amount informed is invalid.")
 
     elif option == "w":
         value = float(input("Inform withdraw value: "))
 
-        exceded_balance = value > balance
+        exceeded_balance = value > balance
+        exceeded_limit = value > limit
+        exceeded_withdraw = withdraw_number >= WITHDRAW_LIMIT
 
-        exceded_limit = value > limit
-
-        exceded_withdraw = withdraw_number >= WITHDRAW_LIMIT
-
-        if exceded_balance:
+        if exceeded_balance:
             print("Operation failed! Insufficient funds.")
-
-        elif exceded_limit:
-            print("Operation failed! The amount exceed the total Balance.")
-
-        elif exceded_withdraw:
+        elif exceeded_limit:
+            print("Operation failed! The amount exceeds the limit.")
+        elif exceeded_withdraw:
             print("Operation failed! The number of transactions has exceeded for today.")
-
         elif value > 0:
             balance -= value
-            balance += f"Withraw value: U$ {value:.2f}\n"
+            extract += f"Withdraw: U$ {value:.2f}\n"
             withdraw_number += 1
-
+            print("Money Withdrawn successfully, please take your money from the bin.")
         else:
             print("Operation failed! The amount informed is invalid.")
 
     elif option == "e":
         print("\n================ EXTRACT ================")
-        print("NO RECORD OF TRANSACTIONS." if not balance else balance)
+        print("NO RECORD OF TRANSACTIONS." if not extract else extract)
         print(f"\nBalance: U$ {balance:.2f}")
         print("==========================================")
 
@@ -63,4 +57,3 @@ while True:
 
     else:
         print("Invalid operation, please select again one of the options shown")
-
